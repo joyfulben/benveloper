c40d8308aa37f03ca408951986b19c367d8024c5
joyfulben <benfishernow@gmail.com>
2024-05-05 11:53:41 -0600

    still debugging url

--- src/index.js
+++ src/index.js
@@ -1,58 +1,14 @@
-import React from 'react';
-import './index.css';
-import App from './App';
-import ErrorPage from './error-page';
-import Root from './components/PVSystem/Root';
-import Home from './components/PVSystem/Home';
-import SystemForm from './components/PVSystem/SystemForm';
-import Appliances from './components/PVSystem/Appliances';
+// import Root from './components/PVSystem/Root';
+// import Home from './components/PVSystem/Home';
+// import SystemForm from './components/PVSystem/SystemForm';
+// import Appliances from './components/PVSystem/Appliances';
 import ReactDOM from 'react-dom/client';
-import { createHashRouter, RouterProvider } from 'react-router-dom';
-
-const router = createHashRouter([
-  {
-    path:"*",
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  // {
-  //   path:"/",
-  //   element: <App />,
-  //   errorElement: <ErrorPage />,
-  //   children: [
-  //     {
-  //       path: "benveloper",
-  //       element: <App />,
-  //       errorElement: <ErrorPage />
-  //     }
-  //   ]
-  // },
-  {
-    path:"/pv-system",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "system_output",
-        element: <SystemForm />
-      },
-      {
-        path: "e_use_calc",
-        element: <Appliances />
-      }
-    ]
-  }
-]);
+// import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
 
